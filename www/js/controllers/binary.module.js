ec8e3d847228f9ee25e0fc85b7dbb5c252a15cf0
Massih Hazrati <massih@gmail.com>
2015-10-13 02:43:21 -0700

    Updated option page, scripts structure

---
+++ www/js/controllers/binary.module.js
@@ -0,0 +1,10 @@
+/**
+ * @name Binary controller Module
+ * @author Massih Hazrati
+ * @contributors []
+ * @since 10/12/2015
+ * @copyright Binary Ltd
+ * The controller's module of binary app
+ */
+
+angular.module('binary.controllers', []);
