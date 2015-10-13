fb4161ebcc14e6861fab13ac3e9ea933fdf9cd8b
Massih Hazrati <massih@gmail.com>
2015-10-13 02:43:21 -0700

    Updated option page, scripts structure

---
+++ www/js/controllers/trade.controller.js
@@ -0,0 +1,27 @@
+/**
+ * @name TradeController
+ * @author Massih Hazrati
+ * @contributors []
+ * @since 10/12/2015
+ * @copyright Binary Ltd
+ * Handles trade's functionalities
+ */
+
+angular
+	.module('binary.controllers')
+	.controller('TradeController',
+		function($scope, $state) {
+
+			$scope.displayOptions = function($event) {
+				$state.go('options');
+			};
+
+			$scope.purchase = function() {
+				$state.go('contract');
+			};
+
+			$scope.logout = function() {
+				$state.go('signin');
+			};
+
+	});
