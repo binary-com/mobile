2cb436a8726c1fbcb6ec5b277e24a944b3eccd33
Massih Hazrati <massih@gmail.com>
2015-10-14 03:20:52 -0700

    Added websocket factory

--- www/js/controllers/signIn.controller.js
+++ www/js/controllers/signIn.controller.js
@@ -9,14 +9,25 @@
 
 angular
 	.module('binary.controllers')
-	.controller('SignInController',
+	.controller('SignInController', ['$scope',
 		function($scope, $state) {
-
-			$scope.signIn = function() {
-				$state.go('trade');
+			$scope.language = 'EN';
+			$scope.massih = 'hazrati';
+			$scope.signIn = function(token, language) {
+				$scope.tokenError = false;
+				// Validate the token
+				if (token && token.length === 48) {
+					// send it for auth
+					// if auth:
+					// $state.go('trade');
+				} else {
+					// apply error class to the input
+					$scope.tokenError = true;
+				}
 			};
 
 			$scope.goToHelpPage = function() {
 				$state.go('help');
 			};
-	});
+
+	}]);
