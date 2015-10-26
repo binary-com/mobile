8a4ea9f0ed6bebaafbffb18c8d321ddb11b26216
Massih Hazrati <massih@gmail.com>
2015-10-26 02:58:32 -0700

    multiple accounts, general config file

--- www/js/controllers/accounts.controller.js
+++ www/js/controllers/accounts.controller.js
@@ -10,9 +10,39 @@
 angular
 	.module('binary')
 	.controller('AccountsController',
-		function($scope, $state, messageService) {
+		function($scope, $state, websocketService, storageService) {
+			// read list of accounts
+			$scope.accounts = storageService.token.get();
+
+			$scope.removeAccount = function(_token) {
+				storageService.token.remove(_token);
+				$scope.accounts = storageService.token.get();
+			};
+
+			$scope.addAccount = function(_token) {
+				$scope.tokenError = false;
+				// Validate the token
+				if (_token && _token.length === 15) {
+					$scope.newToken = _token;
+					websocketService.send.authentication(_token);
+				} else {
+					// apply error class to the input
+					$scope.tokenError = true;
+				}
+			};
+
+			$scope.$on('authorize', function(e, response) {
+				if (response) {
+					// add it to the token list
+					storageService.token.add($scope.newToken);
+					$scope.accounts = storageService.token.get();
+				} else {
+					$scope.tokenError = true;
+				}
+			});
 
 			$scope.back = function() {
+
 				$state.go('options');
 			};
 	});
