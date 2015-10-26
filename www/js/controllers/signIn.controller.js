d9f07d9ab5ed79558686634c2e351568eccf71bc
Massih Hazrati <massih@gmail.com>
2015-10-26 02:58:32 -0700

    multiple accounts, general config file

--- www/js/controllers/signIn.controller.js
+++ www/js/controllers/signIn.controller.js
@@ -10,9 +10,18 @@
 angular
 	.module('binary')
 	.controller('SignInController',
-		function($scope, $state, websocketService) {
+		function($scope, $state, websocketService, storageService) {
 			$scope.language = 'EN';
+			// on page load if we have a last used token go to trade page directly and do other shits
+			var checkTokens = function() {
+				if (storageService.token.get().length > 0) {
+					// use the last used token
+					$state.go('trade');
+				}
+				console.log('login load');
+			};
 			$scope.signIn = function(token, language) {
+				$scope.token = token;
 				$scope.tokenError = false;
 				// Validate the token
 				if (token && token.length === 15) {
@@ -29,10 +38,15 @@ angular
 
 			$scope.$on('authorize', function(e, response) {
 				if (response) {
+					// add it to the token list
+					console.log('token: ', $scope.token)
+					storageService.token.add($scope.token);
 					$state.go('trade');
 				} else {
 					// empty the token input
 					// 'red' it
 				}
 			});
+
+			//checkTokens();
 	});
