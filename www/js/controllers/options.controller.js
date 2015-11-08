c896d36efba55682e1bed7650ef22ecfa57c67da
Massih Hazrati <massih@gmail.com>
2015-11-08 08:35:41 -0800

    update options page

--- www/js/controllers/options.controller.js
+++ www/js/controllers/options.controller.js
@@ -12,102 +12,102 @@ angular
 	.controller('OptionsController',
 		function($scope, $rootScope, $state, config, websocketService, accountService, tradeService) {
 
-			$rootScope.$on('refresh:options', function(e, response) {
-				$scope.accounts = accountService.getAllAccounts();
-				init();
-			});
-
-			$('.account-select').change(function() {
-				var token = $('.account-select option:selected').val();
-				accountService.validateAccount(token);
-				accountService.setDefaultAccount(token);
-
-				$scope.accounts = accountService.getAllAccounts();
-			});
-
-			var init = function() {
-				// ACCOUNTS
-				$scope.accounts = accountService.getAllAccounts();
-
-				// MARKET & UNDERLYINGS
-				$scope.selectedSymbol = tradeService.getProposal().symbol;
-				$scope.market = tradeService.getMarketForASymbol($scope.selectedSymbol).market;
-				$scope.symbols = tradeService.getAllSymbolsForAMarket($scope.market);
-
-				// TRADE TYPE & TICK & BASIS
-				$scope.tradeType = tradeService.getProposal().contract_type;
-				$scope.ticks = tradeService.getProposal().duration;
-				$scope.basis = tradeService.getProposal().basis;
-
-				// CURRENCY
-				websocketService.sendRequestFor.currencies();
-				$scope.currencies = tradeService.getAllCurrencies();
-				$scope.selectedCurrency = tradeService.getProposal().currency;
-			};
-
-			init();
-
-			// $scope.updateAccount = function(_token) {
-			// 	accountService.validateAccount(_token);
-			// 	accountService.setDefaultAccount(_token);
-			// 	//$scope.selectedAccount = _token;
+			// $rootScope.$on('refresh:options', function(e, response) {
 			// 	$scope.accounts = accountService.getAllAccounts();
-			// 	$scope.selectedAccount = accountService.getDefaultAccount().token;
+			// 	init();
+			// });
+
+			// $('.account-select').change(function() {
+			// 	var token = $('.account-select option:selected').val();
+			// 	accountService.validateAccount(token);
+			// 	accountService.setDefaultAccount(token);
+
+			// 	$scope.accounts = accountService.getAllAccounts();
+			// });
+
+			// var init = function() {
+			// 	// ACCOUNTS
+			// 	$scope.accounts = accountService.getAll();
+
+			// 	// MARKET & UNDERLYINGS
+			// 	$scope.selectedSymbol = tradeService.getProposal().symbol;
+			// 	$scope.market = tradeService.getMarketForASymbol($scope.selectedSymbol).market;
+			// 	$scope.symbols = tradeService.getAllSymbolsForAMarket($scope.market);
+
+			// 	// TRADE TYPE & TICK & BASIS
+			// 	$scope.tradeType = tradeService.getProposal().contract_type;
+			// 	$scope.ticks = tradeService.getProposal().duration;
+			// 	$scope.basis = tradeService.getProposal().basis;
+
+			// 	// CURRENCY
+			// 	websocketService.sendRequestFor.currencies();
+			// 	$scope.currencies = tradeService.getAllCurrencies();
+			// 	$scope.selectedCurrency = tradeService.getProposal().currency;
 			// };
 
-			$scope.updateMarket = function(_market) {
-				$scope.market = _market;
-				$scope.symbols = tradeService.getAllSymbolsForAMarket($scope.market);
-				$scope.selectedSymbol = $scope.symbols[0].symbol;
-				$scope.tradeType = 'CALL';
-			};
+			// init();
 
-			$scope.updateSymbol = function(_selectedSymbol) {
-				$scope.selectedSymbol = _selectedSymbol;
-			};
+			// // $scope.updateAccount = function(_token) {
+			// // 	accountService.validateAccount(_token);
+			// // 	accountService.setDefaultAccount(_token);
+			// // 	//$scope.selectedAccount = _token;
+			// // 	$scope.accounts = accountService.getAllAccounts();
+			// // 	$scope.selectedAccount = accountService.getDefaultAccount().token;
+			// // };
 
-			$scope.updateTradeType = function(_tradeType) {
-				$scope.tradeType = _tradeType;
-			};
+			// $scope.updateMarket = function(_market) {
+			// 	$scope.market = _market;
+			// 	$scope.symbols = tradeService.getAllSymbolsForAMarket($scope.market);
+			// 	$scope.selectedSymbol = $scope.symbols[0].symbol;
+			// 	$scope.tradeType = 'CALL';
+			// };
 
-			$scope.updateTicks = function(_tick) {
-				$scope.ticks = _tick;
-			};
+			// $scope.updateSymbol = function(_selectedSymbol) {
+			// 	$scope.selectedSymbol = _selectedSymbol;
+			// };
 
-			$scope.updateBasis = function(_basis) {
-				$scope.basis = _basis;
-			};
+			// $scope.updateTradeType = function(_tradeType) {
+			// 	$scope.tradeType = _tradeType;
+			// };
 
-			$scope.updateCurrency = function(_currency) {
-				$scope.selectedCurrency = _currency;
-			};
+			// $scope.updateTicks = function(_tick) {
+			// 	$scope.ticks = _tick;
+			// };
 
-			// Navigations
+			// $scope.updateBasis = function(_basis) {
+			// 	$scope.basis = _basis;
+			// };
 
-			$scope.navigateToManageAccounts = function() {
-				$state.go('accounts');
-			};
+			// $scope.updateCurrency = function(_currency) {
+			// 	$scope.selectedCurrency = _currency;
+			// };
 
-			$scope.navigateToTradePage = function() {
-				$state.go('trade');
-			};
+			// // Navigations
 
-			$scope.saveChanges = function() {
-				var proposal = {
-					proposal: 1,
-					symbol: $scope.selectedSymbol,
-					contract_type: $scope.tradeType,
-					duration: $scope.ticks,
-					basis: $scope.basis,
-					currency: $scope.selectedCurrency,
-					amount: tradeService.getAmount(),
-					duration_unit: 't'
-				};
-				tradeService.updateProposal(proposal);
-				tradeService.sendProposal();
+			// $scope.navigateToManageAccounts = function() {
+			// 	$state.go('accounts');
+			// };
 
-				$state.go('trade', {}, { reload: true, inherit: false, notify: true });
-			};
+			// $scope.navigateToTradePage = function() {
+			// 	$state.go('trade');
+			// };
+
+			// $scope.saveChanges = function() {
+			// 	var proposal = {
+			// 		proposal: 1,
+			// 		symbol: $scope.selectedSymbol,
+			// 		contract_type: $scope.tradeType,
+			// 		duration: $scope.ticks,
+			// 		basis: $scope.basis,
+			// 		currency: $scope.selectedCurrency,
+			// 		amount: tradeService.getAmount(),
+			// 		duration_unit: 't'
+			// 	};
+			// 	tradeService.updateProposal(proposal);
+			// 	tradeService.sendProposal();
+
+			// 	$state.go('trade', {}, { reload: true, inherit: false, notify: true });
+			// };
 	});
 
 
