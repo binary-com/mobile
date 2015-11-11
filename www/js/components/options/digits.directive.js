7e693d8474f1d84ae9ca414813b2bc38dd051e92
Massih Hazrati <massih@gmail.com>
2015-11-11 05:10:36 -0800

    updates for options page

---
+++ www/js/components/options/digits.directive.js
@@ -0,0 +1,28 @@
+/**
+ * @name digitsOption
+ * @author Massih Hazrati
+ * @contributors []
+ * @since 11/10/2015
+ * @copyright Binary Ltd
+ */
+
+angular
+	.module('binary')
+	.directive('digitsOption',[
+		'marketService',
+		function(marketService) {
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/components/options/digits.template.html',
+			link: function(scope, element) {
+				scope.digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+				scope.$parent.selected.digit = 0;
+
+				scope.updateDigit = function(_digit) {
+					scope.$parent.selected.digit = _digit;
+					//console.log('selected.digit: ', _digit);
+				};
+
+			}
+		};
+	}]);
