8c89a09f40d61f36f03a1d769676a30133c445dd
Morteza <morteza@binary.com>
2015-12-21 10:48:44 +0800

    cleaning up build directory

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,19 +1,10 @@
-// Ionic Starter App
+/**
+ * @name Binary Module
+ * @author Massih Hazrati
+ * @contributors []
+ * @since 10/12/2015
+ * @copyright Binary Ltd
+ * The main module of binary app
+ */
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+angular.module('binary', ['ionic', 'pascalprecht.translate', 'hmTouchEvents', 'ngIOS9UIWebViewPatch']);
