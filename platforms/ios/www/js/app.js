b2d6eee01da854f21226aa7b6a0c7f580dcc9c48
Morteza <morteza@binary.com>
2016-01-05 15:17:29 +0800

    fixing page reloading in option page

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -12,12 +12,30 @@ angular.module('binary', ['ionic', 'pascalprecht.translate', 'hmTouchEvents', 'n
 
 angular
 	.module('binary')
-	.run(function($ionicPlatform){
+	.run(function($ionicPlatform, $state, alertService){
 		$ionicPlatform.ready(function() {
+            
+            // Setup Google Analytics
             if(typeof analytics !== "undefined") {
                 analytics.startTrackerWithId("UA-40877026-7");
             } else {
                 console.log("Google Analytics Unavailable");
             }
+
+            // Handle the android's hardware button
+            $ionicPlatform.registerBackButtonAction(function(){
+                if($state.current.name === "options"){
+                    alertService.confirmExit(function(res){
+                        if(res == 1)
+                            navigator.app.exitApp();
+                    });
+                }
+                else if($state.current.name === "signin" || $state.current.name === "home" ){
+                    navigator.app.exitApp();
+                }
+                else{
+                    navigator.app.backHistory();
+                }
+            }, 100);
         });
 	});
