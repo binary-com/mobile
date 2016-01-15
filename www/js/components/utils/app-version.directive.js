27620bebed1f9be62a39726b7cce5fb79358a0b7
Morteza Tavanarad <morteza@binary.com>
2016-01-15 07:00:49 +0330

    appVersion in Progress

---
+++ www/js/components/utils/app-version.directive.js
@@ -0,0 +1,27 @@
+/**
+ * @name websocketService
+ * @author Morteza Tavanarad
+ * @contributors []
+ * @since 01/14/2016
+ * @copyright Binary Ltd
+ * Directive to show application version
+ */
+
+angular
+	.module('binary')
+	.directive('appVersion', function($ionicPlatform){
+		return{
+			scope: {},
+			restrict: 'E',
+			templateUrl: 'templates/components/utils/app-version.template.html',
+			link: function(scope){
+				$ionicPlatform.ready(function(){
+					if(cordova){
+						cordova.getAppVersion(function(version){
+							scope.appVersion = version;
+						});
+					}
+				});
+			}
+		}
+	});
