a42a20543489f79e7fa46c64093c4cb4ad43795c
Morteza Tavanarad <morteza@binary.com>
2016-01-28 11:47:07 +0330

    Checking CodePush existance befor use

--- www/js/app.js
+++ www/js/app.js
@@ -50,45 +50,47 @@ angular
 
         // Use codepush to check new update and install it.
         $ionicPlatform.ready(function(){
-            codePush.sync(
-                    function (syncStatus) {
-                        switch (syncStatus) {
-                            // Result (final) statuses
-                            case SyncStatus.UPDATE_INSTALLED:
-                                alertService.displayAlert("Update","The update was installed successfully.");
-                                break;
-                            case SyncStatus.UP_TO_DATE:
-                                console.log("The application is up to date.");
-                                break;
-                            case SyncStatus.UPDATE_IGNORED:
-                                alertService.displayAlert("Update","The user decided not to install the optional update.");
-                                break;
-                            case SyncStatus.ERROR:
-                                alertService.displayAlert("Update","An error occured while checking for updates");
-                                break;
+            if(window.codePush){
+                codePush.sync(
+                        function (syncStatus) {
+                            switch (syncStatus) {
+                                // Result (final) statuses
+                                case SyncStatus.UPDATE_INSTALLED:
+                                    alertService.displayAlert("Update","The update was installed successfully.");
+                                    break;
+                                case SyncStatus.UP_TO_DATE:
+                                    console.log("The application is up to date.");
+                                    break;
+                                case SyncStatus.UPDATE_IGNORED:
+                                    alertService.displayAlert("Update","The user decided not to install the optional update.");
+                                    break;
+                                case SyncStatus.ERROR:
+                                    alertService.displayAlert("Update","An error occured while checking for updates");
+                                    break;
 
-                                // Intermediate (non final) statuses
-                            case SyncStatus.CHECKING_FOR_UPDATE:
-                                console.log("Checking for update.");
-                                break;
-                            case SyncStatus.AWAITING_USER_ACTION:
-                                console.log("Alerting user.");
-                                break;
-                            case SyncStatus.DOWNLOADING_PACKAGE:
-                                console.log("Downloading package.");
-                                break;
-                            case SyncStatus.INSTALLING_UPDATE:
-                                console.log("Installing update");
-                                break;
-                        }
-                    },
-            {
-                installMode: InstallMode.IMMEDIATE, updateDialog: true
-            },
-            function (downloadProgress) {
-                console.log("Downloading " + downloadProgress.receivedBytes + " of " + downloadProgress.totalBytes + " bytes.");
+                                    // Intermediate (non final) statuses
+                                case SyncStatus.CHECKING_FOR_UPDATE:
+                                    console.log("Checking for update.");
+                                    break;
+                                case SyncStatus.AWAITING_USER_ACTION:
+                                    console.log("Alerting user.");
+                                    break;
+                                case SyncStatus.DOWNLOADING_PACKAGE:
+                                    console.log("Downloading package.");
+                                    break;
+                                case SyncStatus.INSTALLING_UPDATE:
+                                    console.log("Installing update");
+                                    break;
+                            }
+                        },
+                {
+                    installMode: InstallMode.IMMEDIATE, updateDialog: true
+                },
+                function (downloadProgress) {
+                    console.log("Downloading " + downloadProgress.receivedBytes + " of " + downloadProgress.totalBytes + " bytes.");
+                }
+                );
             }
-            );
         });
     });
 });
