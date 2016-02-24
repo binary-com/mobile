01edafd538ee42dccbfc01cf2332801bae39b6e3
Amin <marashi.s.amin@gmail.com>
2016-02-24 10:09:08 +0330

    Debugged both scripts

--- json2po.sh
+++ json2po.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install translation toolkit first: http://translate-toolkit.readthedocs.org/en/latest/index.html
+#set -x
 shopt -s extglob
-included_languages="ar,de,en,es,fr,id,it,pl,pt,ru,vi,zh_cn,zh_tw,it"
 if [ ! -e "po2json.sh" ]; then
 	echo "Error: This script needs the po2json.sh as its dependency"
 	exit 1
@@ -30,13 +30,14 @@ if [ $stashed -eq 1 ]; then
 	git stash pop
 fi
 
-./po2json.sh "$included_languages" "$tmp_file/old_po"
+./po2json.sh "$tmp_file/old_po"
 json2po -t "$tmp_file/lang" "$tmp_file/old_po" "$tmp_file/new_po"
 pushd www/translation&&
-	for i in `ls !(en).json`; do msgmerge -U $i "$tmp_file/new_po/$i"; done&&
+	for i in `ls !(en).po`; do msgmerge -U $i "$tmp_file/new_po/$i"; done&&
 popd
 git add www/translation/*.po
 rm www/translation/*.po~
 git commit -m "Updated translation files with the recent changes - `date +'%y%m%d'`"
 
 shopt -u extglob
+#set +x
