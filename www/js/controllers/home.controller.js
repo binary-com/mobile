c378a7e574ab9d95d5c6275cd3ac024ac99a97e1
Morteza Tavanarad <morteza@binary.com>
2016-04-24 14:39:02 +0430

    Adding default value for wsUrl

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -12,10 +12,15 @@ angular
 		function($scope, $state, websocketService, accountService, localStorageService) {
 			var init = function() {
                 $scope.hasWSUrl = true;
-
+                $scope.wsUrl = "wss://www2.binary.com/websockets/v3";
+ 
                 if(!localStorageService.getWSUrl()){
                     $scope.hasWSUrl = false;
-                    return;
+
+                   if(!$scope.$$phase){
+                       $scope.$apply();
+                   }
+                   return;
                 }
 
 				if(typeof(analytics) !== "undefined"){
