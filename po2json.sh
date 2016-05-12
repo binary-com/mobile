e5685989c1e3a4073f24090aa83e46e01c4f9c65
Amin <marashi.s.amin@gmail.com>
2016-05-12 23:22:07 +0430

    Added fix to po2json.sh when all languages should all be selected

--- po2json.sh
+++ po2json.sh
@@ -2,16 +2,19 @@
 # install translation toolkit first: http://translate-toolkit.readthedocs.org/en/latest/index.html
 #set -x
 shopt -s extglob
-completed_langs={$(git log --after="`git log --grep='Updated translation files with the recent changes' -n1 --format='%cd'`" | grep '100.0%' -B2|grep 'Translated using Weblate'|sed 's/^[ a-zA-Z]*(\(.*\))/\1/'| ( completed='';while read line; do export completed="$completed,"$(cd www/translation; grep "$line" -r  -l |cut -d. -f1); done; echo $completed)|sed -rn 's/^.(.*)/\1/p')}
-if [ -z "completed_langs" ];then 
-	echo No recently completed language was detected
-	exit 1
-fi
+completed_langs=$(git log --after="`git log --grep='Updated translation files with the recent changes' -n1 --format='%cd'`" | grep '100.0%' -B2|grep 'Translated using Weblate'|sed 's/^[ a-zA-Z]*(\(.*\))/\1/'| ( completed='';while read line; do export completed="$completed,"$(cd www/translation; grep "$line" -r  -l |cut -d. -f1); done; echo $completed)|sed -rn 's/^.(.*)/\1/p')
 dst="$1"
 if [ -z "$dst" ] ;then
-	mode="INCLUSIVE"
+	mode="INCLUDEPHRASESFROMLANG"
+	if [ -z "$completed_langs" ];then 
+		echo No recently completed language was detected
+		exit 1
+	else
+		completed_langs={$completed_langs}
+	fi
 else
-	mode="EXCLUSIVE"
+	mode="DONOTINCLUDEPHRASESFROMLANG"
+	completed_langs={$(ls *.po| (completed=''; while read line; do completed="$completed,"$(echo $line|cut -d. -f1); done; echo $completed) | sed -rn 's/^.(.*)/\1/p')}
 fi
 function make_new_templates(){
 	cp -r www/i18n "$tmp_file"
@@ -30,15 +33,15 @@ fi
 tmp_file="/tmp/po2json-"`date +'%h%m%d%H%M%S'`
 mkdir "$tmp_file"
 cp -r www/translation "$tmp_file"
-if [ "$mode" == "EXCLUSIVE" ]; then
+if [ "$mode" == "DONOTINCLUDEPHRASESFROMLANG" ]; then
 	make_new_templates
 fi
-if [ "$mode" == "INCLUSIVE" ]; then
+if [ "$mode" == "INCLUDEPHRASESFROMLANG" ]; then
 	git checkout "$destination_branch" 
 	make_new_templates
 fi
 po2json -t "$tmp_file/i18n" "$tmp_file/translation" "$tmp_file"
-if [ "$mode" == "INCLUSIVE" ]; then
+if [ "$mode" == "INCLUDEPHRASESFROMLANG" ]; then
 	eval "cp $tmp_file/$completed_langs.json www/i18n"
 	eval "git add www/i18n/$completed_langs.json"
 	git commit -m "Converted translation files to json - `date +'%y%m%d'`"
