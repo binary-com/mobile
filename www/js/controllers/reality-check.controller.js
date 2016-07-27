c55d18e5a149cdd1cc61e8bbfd9d82eb81d2c4ba
nazaninreihani <nazanin.reyh@gmail.com>
2016-07-27 16:50:42 +0430

    translation and time

--- www/js/controllers/reality-check.controller.js
+++ www/js/controllers/reality-check.controller.js
@@ -1,7 +1,7 @@
 angular
 	.module('binary')
 	.controller('RealityCheckController',
-		function($scope, $rootScope, $state, $timeout, websocketService, appStateService, accountService, $ionicPopup, alertService) {
+		function($scope, $rootScope, $state, $timeout, websocketService, appStateService, accountService, $ionicPopup, alertService, $translate, languageService) {
 			var popupInterval,
 				landingCompanyName;
 
@@ -9,11 +9,11 @@ angular
 				if (!appStateService.isRealityChecked) {
 					landingCompanyName = authorize.landing_company_name;
 					websocketService.sendRequestFor.landingCompanyDetails(landingCompanyName);
-					$scope.$on('landing_company_details', function(e, landingCompanyDetails) {
-						if (landingCompanyDetails.has_reality_check === 1) {
-							$scope.hasRealityCheck();
-						}
-					});
+				}
+			});
+			$scope.$on('landing_company_details', function(e, landingCompanyDetails) {
+				if (landingCompanyDetails.has_reality_check === 1) {
+					$scope.hasRealityCheck();
 				}
 			});
 
@@ -43,21 +43,27 @@ angular
 				appStateService.isRealityChecked = true;
 				$scope.data = {};
 				$scope.data.interval = 60;
-				alertService.displayRealitCheckInterval(
-					'Reality Check',
-					'realitycheck getinterval',
-					$scope,
-					'templates/components/reality-check/interval-popup.template.html', [{
-						text: 'Continue trading',
-						onTap: function(e) {
-							if (!$scope.data.interval) {
-								e.preventDefault();
-							} else {
-								$scope.setInterval($scope.data.interval);
-								$scope.hasRealityCheck();
-							}
+				$translate(['realitycheck.continue', 'realitycheck.title'])
+					.then(
+						function(translation) {
+							alertService.displayRealitCheckInterval(
+								translation['realitycheck.title'],
+								'realitycheck getinterval',
+								$scope,
+								'templates/components/reality-check/interval-popup.template.html', [{
+									text: translation['realitycheck.continue'],
+									onTap: function(e) {
+										if (!$scope.data.interval) {
+											e.preventDefault();
+										} else {
+											$scope.setInterval($scope.data.interval);
+											$scope.hasRealityCheck();
+										}
+									}
+								}, ]);
 						}
-					}, ]);
+					)
+
 			};
 
 			$scope.getLastInterval = function() {
@@ -72,33 +78,51 @@ angular
 			$scope.getRealityCheck = function() {
 				websocketService.sendRequestFor.realityCheck();
 			}
+			$scope.sessionTime = function(reality_check) {
+				$scope.date = reality_check.start_time * 1000;
+				$scope.start_time = new Date($scope.date);
+				$scope.realityCheckitems.start_time = $scope.start_time.toString();
+				$scope.now = new Date();
+				$scope.realityCheckitems.currentTime = $scope.now;
+				$scope.duration = ($scope.now - $scope.date);
+				$scope.realityCheckitems.days = Math.floor($scope.duration / 864e5);
+				$scope.hour = $scope.duration - ($scope.realityCheckitems.days * 864e5);
+				$scope.realityCheckitems.hours = Math.floor($scope.hour / 36e5);
+				$scope.min = $scope.duration - ($scope.realityCheckitems.hours * 36e5);
+				$scope.realityCheckitems.minutes = Math.floor($scope.min / 60000);
+			}
 
 			$scope.alertRealityCheck = function(reality_check) {
+				$scope.realityCheckitems = reality_check;
+				$scope.sessionTime(reality_check);
 				$scope.data = {};
 				$scope.data.interval = parseInt($scope.getInterval('_interval'));
-				$scope.realityCheckitems = reality_check;
-				$scope.realityCheckitems.date = new Date(reality_check.start_time * 1000);
-				$scope.currentTime = new Date().getTime();
 
-				alertService.displayRealityCheckResult(
-					'Reality Check',
-					'realitycheck',
-					$scope,
-					'templates/components/reality-check/reality-check-result.template.html', [{
-						text: 'Log Out',
-						onTap: function() {
-							alertService.confirmRemoveAllAccount();
+				$translate(['realitycheck.title', 'realitycheck.continue', 'realitycheck.logout'])
+					.then(
+						function(translation) {
+							alertService.displayRealityCheckResult(
+								translation['realitycheck.title'],
+								'realitycheck',
+								$scope,
+								'templates/components/reality-check/reality-check-result.template.html', [{
+									text: translation['realitycheck.logout'],
+									onTap: function() {
+										alertService.confirmRemoveAllAccount();
+									}
+								}, {
+									text: translation['realitycheck.continue'],
+									onTap: function() {
+										if (sessionStorage._interval) {
+											$scope.getLastInterval();
+										} else {
+											$scope.data.interval = 60;
+										}
+										$scope.hasRealityCheck();
+									}
+								}, ]);
 						}
-					}, {
-						text: 'Continue trading',
-						onTap: function() {
-							if (sessionStorage._interval) {
-								$scope.getLastInterval();
-							} else {
-								$scope.data.interval = 60;
-							}
-							$scope.hasRealityCheck();
-						}
-					}, ]);
+					)
+
 			};
 		});
