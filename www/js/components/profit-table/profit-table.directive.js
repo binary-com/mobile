554d669c6529eac4b6268b4525d1084ca48cce54
nazaninreihani <nazanin.reyh@gmail.com>
2016-08-31 11:48:04 +0430

    date filters and appId filter added

--- www/js/components/profit-table/profit-table.directive.js
+++ www/js/components/profit-table/profit-table.directive.js
@@ -13,12 +13,14 @@ angular
 					scope.profitTableSet = false;
 					scope.appID = 'allApps';
 					scope.dateType = 'allTime';
-					scope.itemsPerPage = 5;
+					scope.itemsPerPage = 10;
 					scope.itemsFirstCall = scope.itemsPerPage + 1;
 					scope.currentPage = 0;
 					scope.nextPageDisabled = true;
 					scope.prevPageDisabled = true;
+					scope.customDateEnabled = false;
 					scope.transactions = [];
+					// send request for profit table for the first time
 					if (!scope.profitTableSet) {
 						var params = {
 							"limit": scope.itemsFirstCall,
@@ -26,23 +28,24 @@ angular
 						}
 						websocketService.sendRequestFor.profitTable(params);
 					}
-
+					// previous button
 					scope.prevPage = function() {
 						if (scope.currentPage > 0) {
 							scope.currentPage--;
 						}
 					};
-
+					// next button
 					scope.nextPage = function() {
 						scope.currentPage++;
 					};
-
+					// set currentPage
 					scope.setPage = function(n) {
 						if (n > 0) {
 							scope.currentPage = n;
 						}
 					};
-
+					// watch currentPage for changing the pages via next or prev button
+					// whenever changed send request for new data
 					scope.$watch("currentPage", function(newValue, oldValue) {
 						var params = {
 							"description": 1,
@@ -51,37 +54,113 @@ angular
 						}
 						websocketService.sendRequestFor.profitTable(params);
 					});
+					scope.$watch("dateType", function() {
+						scope.currentPage = 0;
 
+						scope.$applyAsync(function() {
+							if (scope.dateType == 'customDate') {
+								scope.customDateEnabled = true;
+							} else {
+								scope.customDateEnabled = false;
+							}
+						});
 
+						if (scope.dateType == 'allTime') {
+							scope.dateFrom = "";
+							scope.dateTo = "";
+						} else if (scope.dateType == 'monthAgo') {
+							var now = new Date();
+							var current = now.getTime();
+							var dayBeforeDate = now.setDate(now.getDate() - 30);
+							scope.dateFrom = dayBeforeDate;
+							scope.dateTo = "";
+						} else if (scope.dateType == 'sevenDayAgo') {
+							var now = new Date();
+							var current = now.getTime();
+							var dayBeforeDate = now.setDate(now.getDate() - 7);
+							scope.dateFrom = dayBeforeDate;
+							scope.dateTo = "";
+						} else if (scope.dateType == 'threeDayAgo') {
+							var now = new Date();
+							var current = now.getTime();
+							var dayBeforeDate = now.setDate(now.getDate() - 3);
+							scope.dateFrom = dayBeforeDate;
+							scope.dateTo = "";
+						} else if (scope.dateType == 'oneDayAgo') {
+							var now = new Date();
+							var current = now.getTime();
+							var dayBeforeDate = now.setDate(now.getDate() - 1);
+							scope.dateFrom = dayBeforeDate;
+							scope.dateTo = "";
+						}
+
+						var params = {
+							"description": 1,
+							"limit": scope.itemsFirstCall,
+							"offset": scope.currentPage,
+							"date_from": scope.dateFrom,
+							"date_to": scope.dateTo
+						}
+						websocketService.sendRequestFor.profitTable(params);
+
+					});
+
+					scope.setCustomDate = function() {
+						scope.currentPage = 0;
+						if (scope.startFrom && scope.endTo) {
+							var startDate = document.getElementById("datefrom").value;
+							startDate = new Date(startDate).getTime() / 1000;
+							var finishDate = document.getElementById("dateto").value;
+							finishDate = new Date(finishDate).getTime() / 1000;
+						}
+						scope.dateFrom = startDate || "";
+						scope.dateTo = finishDate || "";
+						var params = {
+							"description": 1,
+							"limit": scope.itemsFirstCall,
+							"offset": scope.currentPage,
+							"date_from": scope.dateFrom,
+							"date_to": scope.dateTo
+						}
+						websocketService.sendRequestFor.profitTable(params);
+					}
+
+					// do this on response of any profitTable request
 					scope.$on('profit_table:update', function(e, _profitTable, _passthrough) {
 						scope.transactions.length = 0;
 						scope.profitTableSet = true;
 						scope.profitTable = _profitTable;
-						scope.items = scope.profitTable.transactions;
 						scope.count = scope.profitTable.count;
-						if (scope.currentPage == 0) {
-							scope.$applyAsync(function() {
-								scope.prevPageDisabled = true;
-							});
-						} else if (scope.currentPage != 0) {
-							scope.$applyAsync(function() {
-								scope.prevPageDisabled = false;
-							});
-						}
-						if (scope.count < scope.itemsFirstCall) {
-							scope.transactions = scope.items;
-							scope.$applyAsync(function() {
-								scope.nextPageDisabled = true;
-							});
-						} else {
-							scope.items.forEach(function(el, i) {
-								if (i < scope.count - 1) {
-									scope.transactions.push(scope.items[i]);
-									scope.$applyAsync(function() {
-										scope.nextPageDisabled = false;
-									});
-								}
-							});
+						if (scope.profitTable.transactions) {
+							scope.items = scope.profitTable.transactions;
+							// enable and disabling previous button
+							if (scope.currentPage == 0) {
+								scope.$applyAsync(function() {
+									scope.prevPageDisabled = true;
+								});
+							} else if (scope.currentPage != 0) {
+								scope.$applyAsync(function() {
+									scope.prevPageDisabled = false;
+								});
+							}
+							// check if there are still more to show
+							if (scope.count < scope.itemsFirstCall) {
+								scope.transactions = scope.items;
+								scope.$applyAsync(function() {
+									scope.nextPageDisabled = true;
+								});
+							}
+							// check if there are no more transaction to show
+							else {
+								scope.items.forEach(function(el, i) {
+									if (i < scope.count - 1) {
+										scope.transactions.push(scope.items[i]);
+										scope.$applyAsync(function() {
+											scope.nextPageDisabled = false;
+										});
+									}
+								});
+							}
 						}
 					});
 
--- www/js/filters/data-filter.filter.js
+++ www/js/filters/data-filter.filter.js
@@ -1,68 +1,19 @@
-// angular
-// 	.module('binary')
-// 	.filter('dataFilter', ["$filter", function($filter) {
-// 		return function(transactions, appID, dateType, dateFrom, dateTo) {
-// 			var filtered = [],
-// 			customDate = customDate,
-// 			dateFrom = dateFrom,
-// 			dateTo = dateTo,
-// 			appID = appID,
-// 			dateType = dateType;
-//
-// 			for (var i in transactions) {
-// 				var item = transactions[i];
-// 				var purchaseTime = item.purchase_time * 1000;
-// 				var sellTime = item.sell_time * 1000;
-// 				var itemId = item.app_id;
-//
-// 				if(dateType == 'allTime' && (appID == 'allApps' || ((appID == 'tickTradeApp') && itemId == 10))){
-// 					filtered.push(item);
-// 				}	else if (dateType == 'customDate') {
-// 					if (dateFrom && dateTo) {
-// 						var startDate = document.getElementById("datefrom").value;
-// 						startDate = new Date(startDate).getTime();
-// 						var finishDate = document.getElementById("dateto").value;
-// 						finishDate = new Date(finishDate).getTime();
-// 						if (startDate <= purchaseTime && sellTime <= finishDate) {
-// 							filtered.push(item);
-// 						}
-// 					}
-// 				} else if (dateType == 'oneDayAgo' && (appID == 'allApps' || (appID == 'tickTradeApp' && itemId == 10))) {
-// 					var now = new Date();
-// 					var current = now.getTime();
-// 					var dayBeforeDate = now.setDate(now.getDate() - 1);
-// 					if (dayBeforeDate <= purchaseTime) {
-// 						filtered.push(item);
-// 					}
-//
-// 				} else if (dateType == 'threeDayAgo' && (appID == 'allApps' || (appID == 'tickTradeApp' && itemId == 10))) {
-// 					var now = new Date();
-// 					var current = now.getTime();
-// 					var dayBeforeDate = now.setDate(now.getDate() - 3);
-// 					if (dayBeforeDate <= purchaseTime) {
-// 						filtered.push(item);
-// 					}
-//
-// 				} else if (dateType == 'sevenDayAgo' && (appID == 'allApps' || (appID == 'tickTradeApp' && itemId == 10))) {
-// 					var now = new Date();
-// 					var current = now.getTime();
-// 					var dayBeforeDate = now.setDate(now.getDate() - 7);
-// 					if (dayBeforeDate <= purchaseTime) {
-// 						filtered.push(item);
-// 					}
-//
-// 				} else if (dateType == 'monthAgo' && (appID == 'allApps' || (appID == 'tickTradeApp' && itemId == 10))) {
-// 					var now = new Date();
-// 					var current = now.getTime();
-// 					var dayBeforeDate = now.setDate(now.getDate() - 30);
-// 					if (dayBeforeDate <= purchaseTime) {
-// 						filtered.push(item);
-// 					}
-//
-// 				}
-// 			}
-//
-//
-// 			return filtered;
-// 		};
-// 	}]);
+angular
+	.module('binary')
+	.filter('dataFilter', ["$filter", function($filter) {
+		return function(transactions, appID) {
+			var filtered = [],
+			appID = appID,
+			transactions = transactions;
+			for (var i in transactions) {
+				var item = transactions[i];
+				// var purchaseTime = item.purchase_time * 1000;
+				// var sellTime = item.sell_time * 1000;
+				var itemId = item.app_id;
+				if(appID == 'allApps' || ((appID == 'tickTradeApp') && itemId == 10)){
+					filtered.push(item);
+				}
+			}
+			return filtered;
+		};
+	}]);
