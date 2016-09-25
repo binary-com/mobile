16438ff59bcbbb821c3674a9cf533ab7e9e82111
Morteza Tavanarad <tavanarad@gmail.com>
2016-09-25 11:47:13 +0330

    Adding longcode widget, Fixing currency issue in trade page

---
+++ www/js/pages/trade/components/longcode/longcode.controller.js
@@ -0,0 +1,19 @@
+/**
+ * @name longcode controller
+ * @author Morteza Tavnarad
+ * @contributors []
+ * @since 09/27/2016
+ * @copyright Binary Ltd
+ */
+
+(function(){
+  'use strict';
+
+  angular
+    .module('binary.pages.trade.components.longcode.controllers')
+    .controller('LongcodeController', Longcode);
+
+  function Longcode(){
+    var vm = this;
+  }
+})();
