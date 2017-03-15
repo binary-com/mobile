66ab3d3bc8d2794fbe749f5b63e0a491fb36f19f
nazaninreihani <nazanin.reyh@gmail.com>
2017-03-15 15:58:26 +0330

    validate maltainvest account opening and real account opening form

--- www/js/pages/maltainvest-account-opening/maltainvest-account-opening.controller.js
+++ www/js/pages/maltainvest-account-opening/maltainvest-account-opening.controller.js
@@ -95,15 +95,30 @@
                 test: function(val) {
                     if (!vm.isReadonly) {
                         var reg = regex.test(val);
-
-                        if (reg == true) {
-                            return false;
-                        } else {
-                            return true;
-                        }
+                        return reg == true ? false : true;
                     } else {
                         return true;
                     }
+                }
+            }
+        })();
+
+        vm.validateAddress = (function (val) {
+            var regex = /[`~!#$%^&*)(_=+\[}{\]\\";:\?><|]+/;
+            return {
+                test: function (val) {
+                    var reg = regex.test(val);
+                    return reg == true ? false : true;
+                }
+            }
+        })();
+
+        vm.validateSecretAnswer = (function (val) {
+            var regex = /[`~!@#$%^&*)(_=+\[}{\]\\\/";:\?><|]+/;
+            return {
+                test: function (val) {
+                    var reg = regex.test(val);
+                    return reg == true ? false : true;
                 }
             }
         })();
@@ -192,10 +207,13 @@
             vm.params = {};
             _.forEach(vm.data, (value, key) => {
                 if (vm.requestData.indexOf(key) > -1) {
-                    if (key !== 'date_of_birth') {
-                        vm.params[key] = value;
-                    } else {
+                    if (key === 'date_of_birth') {
                         vm.params[key] = $filter('date')(value, 'yyyy-MM-dd');
+
+                    } else if (key === 'address_post_code') {
+                        vm.params[key] = value.trim();
+                    } else {
+                        vm.params[key] = value;
                     }
                 }
             });
