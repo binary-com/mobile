9eacedeac0702e8cdd575ff5c4a5e88ef1c2eea8
nazaninreihani <nazanin.reyh@gmail.com>
2017-03-21 11:58:38 +0330

    disable button when password is updating

--- www/js/pages/change-password/change-password.controller.js
+++ www/js/pages/change-password/change-password.controller.js
@@ -33,12 +33,12 @@
         websocketService.sendRequestFor.changePassword(vm.currentPassword, vm.newPassword);
         vm.newPasswordHasError = false;
         vm.oldPasswordHasError = false;
-        vm.passwrodUpdating = true;
+        vm.passwordUpdating = true;
       }
 
       $scope.$on('change_password:success', (e, change_password) => {
         appStateService.passwordChanged = true;
-        vm.passwrodUpdating = false;
+        vm.passwordUpdating = false;
         $scope.$applyAsync(() => {
           vm.hideInput = true;
           vm.hideChangedPasswordText = false;
@@ -49,7 +49,7 @@
       });
 
       $scope.$on('change_password:error', (e, error) => {
-        vm.passwrodUpdating = false;
+        vm.passwordUpdating = false;
         if (error.hasOwnProperty('details')) {
           if (error.details.hasOwnProperty('new_password')) {
             $scope.$applyAsync(() => {
