0ae8ab3cc900c1f95813828cf5acbf54fb50e17a
ashkan <ashkan.nemati@gmail.com>
2017-08-03 15:25:00 +0800

    Updated translation version

--- translation/js/body.controller.js
+++
@@ -1,6 +0,0 @@
-angular.module("binary").controller("BodyController", function($scope, languageService) {
-    const vm = this;
-    vm.getLanguage = function() {
-        return languageService.read();
-    };
-});
