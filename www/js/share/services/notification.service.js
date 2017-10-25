007f2e24f25325fd4835e89a944d23c4a615505d
nazaninreihani <nazanin.reyh@gmail.com>
2017-10-25 09:51:26 +0330

    fixed translation process in some controllers

--- www/js/share/services/notification.service.js
+++ www/js/share/services/notification.service.js
@@ -13,4 +13,52 @@ angular.module("binary").service("notificationService", function() {
 		 this.notices = [];
     }
 
+    this.messages = {
+        authenticateMessage: {
+            title: "account_authentication",
+            text : "please_authenticate",
+		        link : "authentication"
+        },
+        ageVerificationMessage: {
+            title: "account_age_verification",
+            text : "needs_age_verification",
+            link : "contact"
+        },
+        restrictedMessage: {
+            title: "notifications.account_restriction",
+            text : "notifications.please_contact",
+            link : "contact"
+        },
+        countryNotSetMessage: {
+            title: "account_country",
+            text : "set_country",
+            link : "profile"
+        },
+        financialAssessmentMessage: {
+            title: "financial_assessment_not_completed",
+            text : "complete_financial_assessment",
+            link : "financial-assessment"
+        },
+        taxInformationMessage: {
+            title: "tax_information",
+            text : "complete_profile",
+            link : "profile"
+        },
+        termsAndConditionsMessage: {
+            title: "tnc",
+            text : "accept_tnc",
+            link : "terms-and-conditions"
+        },
+        maxTurnoverLimitNotSetMessage: {
+            title: "max_turnover_limit",
+            text : "set_max_turnover_limit",
+		        link : "self-exclusion"
+        },
+	      currencyNotSetMessage: {
+            title: "account_currency",
+            text : "choose_account_currency",
+            link : "set-currency"
+	      },
+    }
+
 });
