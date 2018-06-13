b5e3841e80371d45f80ef6333da768461b72b924
nazaninreihani <nazanin.reyh@gmail.com>
2018-06-13 12:53:38 +0430

    Fix postcode mandatory

--- www/js/pages/real-account-opening/real-account-opening.controller.js
+++ www/js/pages/real-account-opening/real-account-opening.controller.js
@@ -38,10 +38,8 @@
         const landingCompany = accountService.getDefault().landing_company_name;
         const isVirtual = clientService.isLandingCompanyOf('virtual', landingCompany);
         const accounts = accountService.getAll();
-        const landingCompanyObject = !_.isEmpty(localStorage.landingCompanyObject) ?
-            JSON.parse(localStorage.landingCompanyObject) : {};
-        vm.hasIOM = (landingCompanyObject && landingCompanyObject.financial_company &&
-            landingCompanyObject.financial_company.shortcode === 'iom') ||
+        const upgradableLandingCompanies = appStateService.upgradeableLandingCompanies;
+        vm.hasIOM = _.indexOf(upgradableLandingCompanies, 'iom') > -1 ||
             clientService.hasAccountOfLandingCompany(accounts, 'iom');
         vm.validation = validationService;
         vm.options = accountOptions;
