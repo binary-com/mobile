4449f76cb1fabb5dd28bfab4ce4ff7c3975c34c2
nazaninreihani <nazanin.reyh@gmail.com>
2018-06-19 16:29:24 +0430

    Logic of account categorisation page

--- www/js/pages/account-categorisation/account-categorisation.controller.js
+++ www/js/pages/account-categorisation/account-categorisation.controller.js
@@ -8,22 +8,85 @@
 
 (function() {
     angular
-      .module("binary.pages.account-categorisation.controllers")
-      .controller("AccountCategorisationController", AccountCategorisation);
+        .module("binary.pages.account-categorisation.controllers")
+        .controller("AccountCategorisationController", AccountCategorisation);
 
-    AccountCategorisation.$inject = ["$scope", "$state", "$timeout", "websocketService", "accountService", "clientService", "alertService"];
+    AccountCategorisation.$inject = ["$scope", "$state", "$timeout", "$translate", "websocketService", "accountService", "clientService", "alertService"];
 
-    function AccountCategorisation($scope, $state, $timeout, websocketService, accountService, clientService, alertService) {
+    function AccountCategorisation($scope, $state, $timeout, $translate, websocketService, accountService,
+        clientService, alertService) {
         const vm = this;
-        vm.isDataLoaded = false;
-        vm.userState = '';
+        let loginid = '';
+        const params = {
+            request_professional_status: 0,
+            tax_identification_number  : '',
+            tax_residence              : ''
+        };
 
         const hasValueIn = (arr, val) => _.indexOf(arr, val) > -1;
         const isLandingCompanyOf = (targetLandingCompany, accountLandingCompany) =>
             clientService.isLandingCompanyOf(targetLandingCompany, accountLandingCompany);
 
+        $scope.$on('set-settings:error', (e, error) => {
+            if (error.code === 'InputValidationFailed') {
+                $state.go('profile');
+            } else {
+                alertService.displayError(error.message);
+            }
+        });
+
+        $scope.$on('set-settings', (e, error) => {
+            init();
+        });
+
+        $scope.$on('get_settings', (e, get_settings) => {
+            _.forEach(params, (val, k) => {
+                if (get_settings[k]) params[k] = get_settings[k];
+            });
+            params.request_professional_status = 1;
+            websocketService.sendRequestFor.setAccountSettings(params);
+        });
+
+        $scope.$on('authorize', (e, authorize) => {
+            if (authorize && authorize.loginid !== loginid) {
+                loginid = authorize.loginid;
+                const isFinancial = isLandingCompanyOf('maltainvest', authorize.landing_company_name);
+                if (!isFinancial) {
+                    $state.go('trade');
+                } else {
+                    init();
+                }
+            }
+        });
+
         vm.openProfessionalClientInformation = () => {
             alertService.showProfessioanlClientInformation($scope);
+        };
+
+        vm.updateClientType = () => {
+            websocketService.sendRequestFor.accountSetting();
+        };
+
+        vm.showConfirmProfessionalClient = () => {
+            if (vm.client_type) {
+                alertService.displayProfessionalClientConfirmation(
+                    $translate.instant('professional-client-confirmation.professional_clients'),
+                    'information-popup',
+                    $scope,
+                    'js/share/templates/professional-client/professional-client-confirmation.template.html',
+                    [
+                        {
+                            text : $translate.instant("professional-client-confirmation.decline"),
+                            onTap: () => vm.client_type = 0
+                        },
+                        {
+                            text : $translate.instant("professional-client-confirmation.accept"),
+                            type : "button-positive",
+                            onTap: () => true
+                        }
+                    ]
+                );
+            }
         };
 
         $scope.$on('get_account_status', (e, message) => {
@@ -44,8 +107,11 @@
         });
 
         const init = () => {
+            vm.isDataLoaded = false;
+            vm.userState = '';
             const account = accountService.getDefault();
             if (account) {
+                loginid = account.id;
                 const landingcompany = account.landing_company_name;
                 const isFinancial = isLandingCompanyOf('maltainvest', landingcompany);
                 if (isFinancial) {
@@ -59,6 +125,5 @@
         };
 
         init();
-        // TODO:   also should reinit after set settings and after authorize of changing account
     }
 })();
