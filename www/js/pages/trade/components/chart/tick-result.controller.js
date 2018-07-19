a806c6029cb97e83966d385875b22f98cba3ad9e
nazaninreihani <nazanin.reyh@gmail.com>
2018-07-19 13:45:31 +0430

    fix eslint

--- www/js/pages/trade/components/chart/tick-result.controller.js
+++ www/js/pages/trade/components/chart/tick-result.controller.js
@@ -20,25 +20,25 @@
         vm.selectedTick = 0;
 
         $scope.$on("contract:spot", (e, contract, lastPrice) => {
-                if (vm.reset) {
-                    vm.spots = new Array(contract.duration + 1);
-                    _.map(vm.spots, (val, i) => {
-                        vm.spots[i] = {}
-                    });
-                    vm.reset = false;
-                    vm.counter = 0;
-                }
-                const localContract = _.clone(contract);
-
-                $scope.$applyAsync(() => {
-                    vm.selectedTick = contract.selectedTick - 1;
-                    if (vm.counter < vm.spots.length) {
-                        vm.spots[vm.counter++] = {
-                            result: localContract.result,
-                            value : lastPrice
-                        };
-                    }
+            if (vm.reset) {
+                vm.spots = new Array(contract.duration + 1);
+                _.map(vm.spots, (val, i) => {
+                    vm.spots[i] = {}
                 });
+                vm.reset = false;
+                vm.counter = 0;
+            }
+            const localContract = _.clone(contract);
+
+            $scope.$applyAsync(() => {
+                vm.selectedTick = contract.selectedTick - 1;
+                if (vm.counter < vm.spots.length) {
+                    vm.spots[vm.counter++] = {
+                        result: localContract.result,
+                        value : lastPrice
+                    };
+                }
+            });
         });
 
         $scope.$on("contract:finished", (e, contract) => {
