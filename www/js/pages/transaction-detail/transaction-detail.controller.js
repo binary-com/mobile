2c692c8b438f651a1761c26aa5d424c6976b71c6
nazaninreihani <nazanin.reyh@gmail.com>
2019-06-14 13:11:27 -0400

    show spots in transaction details with correct decimals

--- www/js/pages/transaction-detail/transaction-detail.controller.js
+++ www/js/pages/transaction-detail/transaction-detail.controller.js
@@ -16,11 +16,20 @@
     function TransactionDetail($scope, $timeout, appStateService, websocketService) {
         const vm = this;
         vm.currency = sessionStorage.getItem("currency");
+        vm.fractionalLength = 2;
+        const activeSymbols = JSON.parse(sessionStorage.getItem("all_active_symbols"));
         const id = sessionStorage.getItem("id");
         const contractId = parseInt(id);
         const extraParams = {
             req_id: contractId
         };
+
+        const getFractionalLength =  (floatNumber) => {
+            const stringNumber = floatNumber.toString();
+            const decimalLength = stringNumber.indexOf(".");
+            return stringNumber.length - decimalLength - 1;
+        };
+
         const sendDetailsRequest = () => {
             if (appStateService.isLoggedin) {
                 websocketService.sendRequestFor.openContract(id, extraParams);
@@ -32,10 +41,15 @@
         $scope.$on("proposal:open-contract", (e, proposal_open_contract, req_id) => {
             const proposalOpenContract = proposal_open_contract;
             const reqId = req_id;
+
             if (reqId === contractId) {
-                $scope.$applyAsync(() => {
-                    vm.contract = proposalOpenContract;
-                });
+                const activeSymbol =
+                    activeSymbols.find((activeSymbol) => activeSymbol.symbol === proposalOpenContract.underlying);
+                const pip = activeSymbol.pip || 0.01;
+                vm.fractionalLength = getFractionalLength(pip);
+                vm.contract = proposalOpenContract;
+                $scope.$apply();
+
             }
         });
 
